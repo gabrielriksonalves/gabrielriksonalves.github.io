1333ce191086881599384dd91a4a5cb88d77d526
gabrielriksonalves <93103181+gabrielriksonalves@users.noreply.github.com>
2021-10-24 20:50:02 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var content = document.getElementById('conteudo')
+content.addEventListener('slider', onload)
+
+function slider(){
+    content.style.opacity = "1"
+    content.style.transform = "translateX(0)"
+}
